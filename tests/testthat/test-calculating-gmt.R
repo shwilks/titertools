743ad571b05d9006de615f0aa19c98f6121a5ea5
Sam Wilks <sw463@cam.ac.uk>
2022-09-29 15:18:24 +0200

    Add some tests

--- tests/testthat/test-calculating-gmt.R
+++ tests/testthat/test-calculating-gmt.R
@@ -7,9 +7,33 @@ titers <- t(titerTable(map)[c("B.1.351", "P.1"), srGroups(map) == "D614G"])
 titers1 <- t(titerTable(map)[c("D614G", "B.1.1.7"), srGroups(map) == "D614G", drop = F])
 titers2 <- t(titerTable(map)[c("B.1.351", "P.1"), srGroups(map) == "D614G", drop = F])
 
+log2diff("*", "*", ci_method = "HDI")
+log2diff("<10", "<10", ci_method = "HDI")
+log2diff("10", "*", ci_method = "HDI")
+
 gmt("*")
 gmt(c("40", "*"))
 gmt(c("<10", "<10", "<10"))
+
+titers1 <- c("562.47", "22.02", "<20", "47.23", "21.44", "133.97", "43.64", "<20")
+titers2 <- c("629.29", "<20", "30.58", "28.34", "27.78", "<20", "<20", "21.84")
+
+titers1 <- c("<20", "<20")
+titers2 <- c("<20", "<20")
+
+log2diff(titers1, titers2, dilution_stepsize = 0, ci_method = "HDI")
+stop()
+
+model_sample <- rstan::sampling(
+  stanmodels$gmt,
+  data = standata,
+  init = list(as.list(staninit)),
+  chains = 1,
+  refresh = 0,
+  iter = 10000,
+  warmup = 2000
+)
+
 
 titers_na <- titers1
 titers_na[] <- "*"
