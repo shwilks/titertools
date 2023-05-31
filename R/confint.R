bbfa2248215ff0f55702f60f9c975b974a80f801
Sam Wilks <sw463@cam.ac.uk>
2023-05-31 15:17:52 +0100

    Add tryCatch statement to catch failures to calculate HDI

--- R/confint.R
+++ R/confint.R
@@ -34,53 +34,64 @@ calc_confint <- function(
   output <- cbind(result$par, NA, NA)
   colnames(output) <- c("estimate", "lower", "upper")
 
-  # Set options
-  options <- do.call(sampler_options, options)
+  # Return NA if no result
+  if (sum(!is.na(result$par)) > 0) {
 
-  # Calculate confidence intervals
-  if (method == "quap") {
+    # Set options
+    options <- do.call(sampler_options, options)
 
-    covariance_matrix <- solve(result$hessian)
-    alpha <- 1 - level
-    par_sd <- -diag(covariance_matrix)
-    names(par_sd) <- names(result$par)
+    # Calculate confidence intervals
+    if (method == "quap") {
 
-    ci_output <- do.call(
-      rbind,
-      lapply(pars, function(par) {
-        result$par[par] + qnorm(c(0.5, alpha/2, 1 - alpha/2))*par_sd[par]
-      })
-    )
+      covariance_matrix <- solve(result$hessian)
+      alpha <- 1 - level
+      par_sd <- -diag(covariance_matrix)
+      names(par_sd) <- names(result$par)
 
-  } else if (method %in% c("ETI", "HDI", "BCI", "SI")) {
+      ci_output <- do.call(
+        rbind,
+        lapply(pars, function(par) {
+          result$par[par] + qnorm(c(0.5, alpha/2, 1 - alpha/2))*par_sd[par]
+        })
+      )
 
-    result_sampling <- rstan::sampling(
-      model,
-      data = standata,
-      # init = list(as.list(result$par)),
-      # chains = 1,
-      refresh = 0,
-      iter = options$iter,
-      warmup = options$warmup
-    )
+    } else if (method %in% c("ETI", "HDI", "BCI", "SI")) {
 
-    ci_output <- do.call(
-      rbind,
-      lapply(pars, function(par) {
-        par_sample <- rstan::extract(result_sampling, pars = par)[[1]]
-        par_ci <- bayestestR::ci(par_sample, ci = level, method = method)
-        c(result$par[par], par_ci$CI_low, par_ci$CI_high)
-      })
-    )
+      result_sampling <- rstan::sampling(
+        model,
+        data = standata,
+        init = list(as.list(result$par)),
+        chains = 1,
+        refresh = 0,
+        iter = options$iter,
+        warmup = options$warmup
+      )
 
-  } else {
+      ci_output <- do.call(
+        rbind,
+        lapply(pars, function(par) {
+          tryCatch({
+            par_sample <- rstan::extract(result_sampling, pars = par)[[1]]
+            par_ci <- bayestestR::ci(par_sample, ci = level, method = method)
+            c(result$par[par], par_ci$CI_low, par_ci$CI_high)
+          }, error = function(e) {
+            c(result$par[par], NA, NA)
+          })
+        })
+      )
 
-    stop("method must be one of 'quap', 'ETI', 'HDI', 'BCI' or 'SI'")
+    } else {
+
+      stop("method must be one of 'quap', 'ETI', 'HDI', 'BCI' or 'SI'")
+
+    }
+
+    # Update output
+    output[pars,] <- ci_output
 
   }
 
   # Return output
-  output[pars,] <- ci_output
   rownames(output)[rownames(output) == "mu"] <- "mean"
   rownames(output)[rownames(output) == "sigma"] <- "sd"
   output
